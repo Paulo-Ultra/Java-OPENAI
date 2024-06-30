efbcdbe7c4984be9621be2fddf26b95e88f0b805
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2024-06-30 15:42:45 -0300

    Prompt Template

--- src/main/java/br/com/alura/ecommerce/CategorizadorDeProdutos.java
+++ src/main/java/br/com/alura/ecommerce/CategorizadorDeProdutos.java
@@ -5,27 +5,47 @@ import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
 import com.theokanning.openai.service.OpenAiService;
 
+import javax.swing.plaf.multi.MultiSeparatorUI;
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class CategorizadorDeProdutos {
     public static void main(String[] args) {
 
-        var system= """
-        Você é um categorizador de produtos e deve responder apenas o nome da categoria do produto informado
-        
-        Escolha uma categoria dentre a lista abaixo:
-        
-        1. Higiene Pessoal
-        2. Eletronicos
-        3. Esportes
-        4. Outros
-        
-        ############### exemplo de uso:
-        Pergunta: Bola de futebol
-        Reposta: Esportes        
-        """;
-        var user = "Escova de dentes";
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite as categorias válidas:");
+        var categorias = leitor.nextLine();
+
+        //While usado para teste do prompt template
+        while (true) {
+
+            System.out.println("Digite o nome do produto:");
+            var user = leitor.nextLine();
+
+            var system = """
+                    Você é um categorizador de produtos e deve responder apenas o nome da categoria do produto informado
+                            
+                    Escolha uma categoria dentre a lista abaixo:
+                            
+                    %s
+                            
+                    ############### exemplo de uso:
+                    Pergunta: Bola de futebol
+                    Reposta: Esportes    
+                                    
+                    ######### regras a serem seguidas:     
+                    Caso o usuário pergunte algo que não seja de categorização de produtos, você deve responder que não pode 
+                    ajudar pois o seu papel é apenas responder a categoria dos produtos
+                                    
+                    """.formatted(categorias);
+
+            dispararRequisicao(user, system);
+        }
+
+    }
+
+    public static void dispararRequisicao(String user, String system) {
         var chave = System.getenv("OPENAI_API_KEY");
         var service = new OpenAiService(chave, Duration.ofSeconds(30));
 
@@ -36,13 +56,9 @@ public class CategorizadorDeProdutos {
                         new ChatMessage(ChatMessageRole.USER.value(), user),
                         new ChatMessage(ChatMessageRole.SYSTEM.value(), system)
                 ))
-                .n(5)
                 .build();
         service.createChatCompletion(completionRequest)
                 .getChoices()
-                .forEach(c -> {
-                    System.out.println(c.getMessage().getContent());
-                    System.out.println("---------------------------");
-                });
+                .forEach(c -> System.out.println(c.getMessage().getContent()));
     }
 }
